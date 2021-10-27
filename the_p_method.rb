fd45b90b591545275c6f444b1fd1bc692264fb44
Joyce Kelly <joyccekellywork@gmail.com>
2021-10-27 10:25:10 -0300

    the puts and p method

---
+++ the_p_method.rb
@@ -0,0 +1,14 @@
+puts "steen seagal"
+p "steven seagal"
+
+puts "Hi there, this is a
+big line break"
+
+p "hi there, this is a 
+big line break"
+
+#p basically offers the same output as puts or print, it does operate more similary
+#to puts because it does add a line break to the end of the output, but it also
+#provide us additional details about the thing that we are outputting 
+
+p 5; puts 5
---
+++ the_print_method.rb
@@ -0,0 +1,7 @@
+puts "hello"
+puts "world"
+
+print "hello"
+print " "
+print "world" #the print method not have spaces or break lines
+
