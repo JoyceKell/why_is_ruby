40bd951038cb0ba31a22f862ad6fdd5140054b3b
Joyce Kelly <joyccekellywork@gmail.com>
2021-10-27 10:02:27 -0300

    first commit

---
+++ the_puts_method.rb
@@ -0,0 +1,15 @@
+puts "hello world"
+puts "i'm alive and well!"
+
+#ruby add a line break which allows the next text begin on the very next line
+
+puts "hello world"; puts "i'm alive and well!" #another way to write this command
+
+puts 5 #numbers not need to ""
+puts "5" # this is diferent to 5
+
+puts 4+5 #ruby print the result
+puts "4"+"3" #ruby just combine this strings, result=43
+
+#the puts method automatically adds a line break at the end of every output that you do
+puts #just the method puts, wihout any instruction, print a break line
